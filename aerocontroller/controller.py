6273565cf5342d26934359110088aad95aaa8c18
Ben Schreck <benjaminschreck93@gmail.com>
2021-07-18 12:20:23 -0700

    initial version

--- aerocontroller/controller.py
+++ aerocontroller/controller.py
@@ -7,87 +7,110 @@ from gpiozero import LED
 import time
 from twilio.rest import Client as TwilioClient
 import math
+from dotenv import load_dotenv
+import os
 
-load_dotenv(dotenv_path=env_path)
-TWILIO_SID = os.environ.get("TWILIO_SID")
-TWILIO_TOKEN = os.environ.get("TWILIO_TOKEN")
-TWILIO_FROM_NUMBER = os.environ.get("TWILIO_FROM_NUMBER")
-TWILIO_TO_NUMBER = os.environ.get("TWILIO_TO_NUMBER")
-TWILIO_CLIENT = TwilioClient(twilio_sid, twilio_token)
-LOW_HUMIDITY_THRESHOLD = int(os.environ.get("LOW_HUMIDITY_THRESHOLD"))
-HIGH_HUMIDITY_THRESHOLD = int(os.environ.get("LOW_HUMIDITY_THRESHOLD"))
-LOW_TEMP_THRESHOLD = int(os.environ.get("LOW_TEMP_THRESHOLD"))
-HIGH_TEMP_THRESHOLD = int(os.environ.get("HIGH_TEMP_THRESHOLD"))
-LED_START_HOUR = int(os.environ.get("LED_START_HOUR"))
-LED_END_HOUR = int(os.environ.get("LED_END_HOUR"))
-OUTPUMP_START_HOUR_START_HOUR = int(os.environ.get("OUTPUMP_START_HOUR"))
-OUTPUMP_END_HOUR = int(os.environ.get("OUTPUMP_END_HOUR"))
-INPUMP_ON_SECONDS = int(os.environ.get("INPUMP_ON_SECONDS"))
+class AeroController:
+    def __init__(self, env_path):
+        self.env_path = env_path
+        load_dotenv(dotenv_path=self.env_path)
+        self.twilio_sid = os.environ.get("TWILIO_SID")
+        self.twilio_token = os.environ.get("TWILIO_TOKEN")
+        self.twilio_from_number = os.environ.get("TWILIO_FROM_NUMBER")
+        self.twilio_to_number = os.environ.get("TWILIO_TO_NUMBER")
+        self.twilio_client = TwilioClient(self.twilio_sid, self.twilio_token)
 
-SI7021 = SI7021()
-LIGHT = LED('BOARD11')
-INPUMP = LED('BOARD18')
-OUTPUMP = LED('BOARD16')
+        self.low_humidity_threshold = int(os.environ.get("LOW_HUMIDITY_THRESHOLD"))
+        self.high_humidity_threshold = int(os.environ.get("HIGH_HUMIDITY_THRESHOLD"))
+        self.low_temp_threshold = int(os.environ.get("LOW_TEMP_THRESHOLD"))
+        self.high_temp_threshold = int(os.environ.get("HIGH_TEMP_THRESHOLD"))
+        self.led_start_hour = int(os.environ.get("LED_START_HOUR"))
+        self.led_end_hour = int(os.environ.get("LED_END_HOUR"))
+        self.outpump_start_hour = int(os.environ.get("OUTPUMP_START_HOUR"))
+        self.outpump_end_hour = int(os.environ.get("OUTPUMP_END_HOUR"))
+        self.inpump_on_seconds = int(os.environ.get("INPUMP_ON_SECONDS"))
 
-def turn_light_on():
-    LIGHT.off()
-def turn_light_off():
-    LIGHT.on()
-def turn_inpump_on():
-    INPUMP.off()
-def turn_inpump_off():
-    INPUMP.on()
-def cycle_inpump():
-    if INPUMP_STATE:
-        turn_inpump_off()
-    else:
-        turn_inpump_oon()
-def cycle_outpump():
-    if INPUMP_STATE:
-        OUTPUMP.off()()
-    else:
-        OUTPUMP.on()
+        self.si7021 = SI7021()
+        self.light = LED('BOARD11')
+        self.inpump = LED('BOARD18')
+        self.outpump = LED('BOARD16')
+        self.inpump_state = False
+        self.outpump_state = False
 
+    def turn_light_on(self):
+        self.light.off()
 
-def send_text(msg):
-    twilio_client.messages.create(
-        to=f"+1{twilio_to_number}",
-        from_=f"+1{twilio_from_number}",
-        body=msg
-    )
+    def turn_light_off(self):
+        self.light.on()
 
-while True:
-    temp = SI7021.get_temp('F')
-    print("TEMP F : ", temp)
-    if temp < LOW_TEMP_THRESHOLD or temp > HIGH_TEMP_THRESHOLD:
-        send_text(f"Temperature alert: {math.round(temp)}%")
-    humidity = SI7021.get_humidity()
-    print("HUMIDITY: ", humidity)
-    if humidity < LOW_HUMIDITY_THRESHOLD or humidity > HIGH_HUMIDITY_THRESHOLD:
-        send_text(f"Humidity alert: {math.round(humidity)}%")
+    def turn_inpump_on(self):
+        self.inpump.off()
 
-    hour = dt.datetime.today().hour
+    def turn_inpump_off(self):
+        self.inpump.on()
 
-    print("HOUR = ", hour)
-    if hour >= LED_START_HOUR and hour <= LED_END_HOUR
-        turn_light_on()
-        send_text("turning on LED")
-    else:
-        turn_light_off()
-        send_text("turning off LED")
+    def turn_outpump_on(self):
+        self.outpump.on()
 
-    if hour >= OUTPUMP_START_HOUR and hour <= OUTPUMP_END_HOUR:
-        turn_outpump_on()
-        send_text("turning on outpump")
-    else:
-        turn_outpump_off()
-        send_text("turning off outpump")
+    def turn_outpump_off(self):
+        self.outpump.off()
 
-    second = dt.datetime.today().seoond
-    if second <= INPUMP_ON_SECONDS:
-        turn_inpump_on()
-    else:
-        turn_inpump_off()
+    def cycle_inpump(self):
+        if self.inpump_state:
+            self.turn_inpump_off()
+        else:
+            self.turn_inpump_oon()
 
-    time.sleep(1)
-    sys.stdout.flush()
+    def cycle_outpump(self):
+        if self.inpump_state:
+            self.outpump.off()()
+        else:
+            self.outpump.on()
+
+    def send_text(self, msg):
+        self.twilio_client.messages.create(
+            to=f"+1{self.twilio_to_number}",
+            from_=f"+1{self.twilio_from_number}",
+            body=msg
+        )
+
+    def run(self):
+        temp = self.si7021.get_temp('F')
+        print("TEMP F : ", temp)
+        if temp < self.low_temp_threshold or temp > self.high_temp_threshold:
+            self.send_text(f"Temperature alert: {round(temp)}%")
+        humidity = self.si7021.get_humidity()
+        print("HUMIDITY: ", humidity)
+        if humidity < self.low_humidity_threshold or humidity > self.high_humidity_threshold:
+            self.send_text(f"Humidity alert: {round(humidity)}%")
+
+        hour = dt.datetime.today().hour
+
+        print("HOUR = ", hour)
+        if hour >= self.led_start_hour and hour <= self.led_end_hour:
+            self.turn_light_on()
+            self.send_text("turning on LED")
+        else:
+            self.turn_light_off()
+            self.send_text("turning off LED")
+
+        if hour >= self.outpump_start_hour and hour <= self.outpump_end_hour:
+            self.turn_outpump_on()
+            self.send_text("turning on outpump")
+        else:
+            self.turn_outpump_off()
+            self.send_text("turning off outpump")
+
+        second = dt.datetime.today().second
+        if second <= self.inpump_on_seconds:
+            self.turn_inpump_on()
+        else:
+            self.turn_inpump_off()
+
+        time.sleep(1)
+        sys.stdout.flush()
+
+if __name__ == '__main__':
+    controller = AeroController('.env')
+    while True:
+        controller.run()
--- aerocontroller/tests.py
+++ aerocontroller/tests.py
@@ -9,8 +9,8 @@ sys.modules['RPi'] = fake_rpi.RPi     # Fake RPi
 sys.modules['RPi.GPIO'] = fake_rpi.RPi.GPIO # Fake GPIO
 sys.modules['smbus'] = fake_rpi.smbus # Fake smbus (I2C)
 
-from kojifier.utils import parse_time, parse_temp, LEDReverseWrapper
-from kojifier.auto_fermenter import AutoFermenter
+from aerocontroller.utils import parse_time, parse_temp, LEDReverseWrapper
+from aerocontroller.controller import AeroController
 from unittest.mock import call
 from unittest import mock
 import numpy as np
@@ -41,43 +41,6 @@ def test_parse_time(t, expected):
 def test_parse_temp(t, unit, expected):
     assert parse_temp(t, unit) == expected
 
-class MockW1ThermSensor:
-    def __init__(self, fermenter):
-        self.calls = 0
-        self.fermenter = fermenter
-        self.dts = []
-        self.temps = []
-        self.powers = []
-        self.time_since_cooling = 0
-        self.previous_time = None
-    def get_temp(self, degrees):
-        if self.calls < 20:
-            self.calls += 1
-            self.temp = 130
-            return self.temp
-        elif self.calls == 20:
-            self.calls += 1
-            self.temp = 88
-            return self.temp
-        self.calls += 1
-        self.fermenter.pid.reset()
-
-        min_temp = 75
-        max_temp = 500
-        p = self.fermenter.new_hot_plate_power
-        current_time = time.time()
-        dt = current_time - (self.previous_time or current_time)
-        self.previous_time = current_time
-        if p == 0:
-            if self.temp > min_temp:
-                self.temp -= .02 * dt
-        else:
-            if self.temp < max_temp:
-                self.temp += p * dt
-        self.powers.append(p)
-        self.temps.append(self.temp)
-        return self.temp
-
 
 class MockLED:
     def __init__(self, pin):
@@ -96,17 +59,24 @@ class MockTwilioClient:
         self.calls = 0
     def create(self, to, from_, body):
         if self.calls == 0:
-            assert 'Starting to incubate' in body
+            assert 'turning on LED' in body
         elif self.calls == 1:
-            assert 'done' in body
+            assert 'turning on outpump' in body
         else:
             raise ValueError("too many calls")
         self.calls += 1
 
+class MockSI7021:
+    def get_temp(self, unit):
+        return 70
+    def get_humidity(self):
+        return 40
 
-@mock.patch('kojifier.utils.LED', side_effect=MockLED)
-@mock.patch('kojifier.auto_fermenter.W1SensorWrapper', side_effect=MockW1ThermSensor)
-def test_auto_fermenter_tempeh(mock_led, mock_sensor):
-    fermenter = AutoFermenter(config='kojifier/test_tempeh_config.yaml', env_path='kojifier/.env.test')
-    with mock.patch.object(fermenter, 'twilio_client', MockTwilioClient('sid', 'token')):
-        fermenter.make_tempeh()
+
+@mock.patch('aerocontroller.controller.LED', side_effect=MockLED)
+def test_auto_fermenter_tempeh(mock_led):
+    mock_si7021 = mock.patch("aerocontroller.controller.SI7021", MockSI7021)
+    mock_twilio_client = mock.patch("aerocontroller.controller.TwilioClient", MockTwilioClient)
+    with mock_si7021, mock_twilio_client:
+        controller = AeroController(env_path='aerocontroller/.env.test')
+        controller.run()
